765762159b388fd10fefaa7f8187c9513ee2c24c
Helmatron <158586047+Helmatron@users.noreply.github.com>
2024-05-01 19:24:00 +0200

    Creazione classe e metodi - inizio di collegamento al main

--- src/main/java/org/lessons/java/geometria/Main.java
+++ src/main/java/org/lessons/java/geometria/Main.java
@@ -1,10 +1,19 @@
 package org.lessons.java.geometria;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
 		
+		Scanner input = new Scanner(System.in);
 
+		Rettangolo dati = new Rettangolo(0, 0);
+		
+		int base =	Rettangolo.setBase();
+		
+		
+		
 	}
 
 }
--- src/main/java/org/lessons/java/geometria/Rettangolo.java
+++ src/main/java/org/lessons/java/geometria/Rettangolo.java
@@ -1,9 +1,47 @@
 package org.lessons.java.geometria;
 
-public class Rettangolo {
+import java.util.Scanner;
 
-	public Rettangolo() {
+import java.security.PublicKey;
+
+public class Rettangolo {
+	
+	
+	private int base;
+	private int altezza;
+
+	public Rettangolo(int base, int altezza) {
+			this.base = base;
+			this.altezza = altezza;
+	}
+
+	public void setBase() {
+		if (this.base == 0) {
+			this.base = scanner.nextInt();
+		} else {
+			this.base = 0;
+		}
 		
 	}
 
+	public void setAltezza(int nuovaAltezza) {
+		if (nuovaAltezza != 0) {
+			this.altezza = nuovaAltezza;
+		}
+	}
+
+	public void calcoloArea() {
+		if (this.base != 0 && this.altezza != 0) {
+			int area = this.base * this.altezza;
+			System.out.println("L'area del rettangolo è: " + area);
+		}
+	}
+	
+	public void calcoloPerimetro() {
+		if (this.base != 0 && this.altezza != 0) {
+			int perimetro = (this.base * 2) + (this.altezza * 2);
+			System.out.println("Il perimetro del rettangolo è: " + perimetro);
+		}
+	}
+	
 }
